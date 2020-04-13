a865fe79c5c86b8b4803e0975295d8f7dc7fdaff
Khoa Dien <khoa.dien@hotmail.com>
2020-04-13 11:59:11 -0300

    Add PowerShell profile

---
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,52 @@
+if (!(Test-Path Alias:npp)) {
+    New-Alias -Name npp -Value "notepad++.exe"
+}
+
+Set-Alias -Name curl -Value curl.exe -Option AllScope
+
+Import-Module posh-git
+#Import-Module posh-git,oh-my-posh
+#Set-Theme Paradox
+
+function curlx {
+    curl.exe -x http://proxy.example.com:8080 --url $args[0]
+}
+
+function e. {
+    explorer .
+}
+
+##########################
+## Custom Git functions ##
+##########################
+function glog {
+    git log --graph --decorate --oneline
+}
+
+function gs {
+    git status
+}
+
+function gtop {
+    $gitRoot = git rev-parse --show-toplevel
+    Set-Location $gitRoot
+}
+
+function git_ssh {
+    $newRemoteUrl = (git remote get-url origin).Replace("https://kdien@bitbucket.org/", "git@bitbucket.org:")
+    git remote set-url origin $newRemoteUrl
+}
+
+function git_https {
+    $newRemoteUrl = (git remote get-url origin).Replace("git@bitbucket.org:", "https://kdien@bitbucket.org/")
+    git remote set-url origin $newRemoteUrl
+}
+
+function Set-GitProxy {
+    git config --global http.proxy http://proxy.example.com:8080
+}
+
+function Unset-GitProxy {
+    git config --global --unset http.proxy
+}
+
