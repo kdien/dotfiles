0d1650a96c317b41a987f93f75dc607f58f1975d
Khoa Dien <khoa.dien@alc.ca>
2021-01-27 15:30:02 -0400

    Update PowerShell theme toggle script to set both system and app themes

--- powershell/toggleWindowsTheme.ps1
+++ powershell/toggleWindowsTheme.ps1
@@ -1,9 +1,10 @@
 $path = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize'
 
-if ( (Get-ItemProperty -Path $path -Name AppsUseLightTheme).AppsUseLightTheme -eq 0 ) {
+if ( (Get-ItemProperty -Path $path -Name SystemUsesLightTheme).SystemUsesLightTheme -eq 0 ) {
     $value = '1'
 } else {
     $value = '0'
 }
 
 Set-ItemProperty -Path $path -Name AppsUseLightTheme -Value $value
+Set-ItemProperty -Path $path -Name SystemUsesLightTheme -Value $value
