e0da6f02690301f92ba11155131a806cd3e03ebc
Khoa Dien <khoa.dien@bounteous.com>
2021-11-24 12:16:22 -0400

    Update all configs to replace MesloLGS with MesloLGSM

--- hyper/.hyper.js
+++ hyper/.hyper.js
@@ -12,7 +12,7 @@ module.exports = {
     fontSize: 15,
 
     // font family with optional fallbacks
-    fontFamily: '"MesloLGS Nerd Font", Consolas, "Lucida Console", monospace',
+    fontFamily: '"MesloLGM Nerd Font", Consolas, "Lucida Console", monospace',
 
     // default font weight: 'normal' or 'bold'
     fontWeight: 'normal',
