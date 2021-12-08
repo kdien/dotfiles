44f544254af6664266e22d2327f43f94a30b36c0
Khoa Dien <khoa.dien@bounteous.com>
2021-12-08 14:52:54 -0400

    Reorganize and add user.js for firefox

---
+++ firefox/user.js
@@ -0,0 +1,5 @@
+user_pref("browser.tabs.inTitlebar", 1);
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("browser.compactmode.show", true);
+user_pref("gfx.font_rendering.cleartype_params.enhanced_contrast", 25);
+user_pref("gfx.font_rendering.cleartype_params.force_gdi_classic_for_families", "");
