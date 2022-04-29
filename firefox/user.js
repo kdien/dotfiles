3e92fd8e892f5fb8c26bd509712602e217b85451
Khoa Dien <khoa.dien@bounteous.com>
2022-04-29 13:56:21 -0300

    Add fix for Firefox bold font rendering in Windows

--- firefox/user.js
+++ firefox/user.js
@@ -1,5 +1,4 @@
-user_pref("browser.tabs.inTitlebar", 1);
-user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
-user_pref("browser.compactmode.show", true);
+// Fix Windows font rendering
+user_pref("gfx.font_rendering.directwrite.bold_simulation", 2);
 user_pref("gfx.font_rendering.cleartype_params.enhanced_contrast", 25);
 user_pref("gfx.font_rendering.cleartype_params.force_gdi_classic_for_families", "");
