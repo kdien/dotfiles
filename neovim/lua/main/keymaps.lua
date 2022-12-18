8571feee0346d2de59a3f27e0cb57456ff80dc0e
Khoa Dien <khoa.dien@hotmail.com>
2022-12-18 15:14:06 -0400

    Add useful neovim keymaps

--- neovim/lua/main/keymaps.lua
+++ neovim/lua/main/keymaps.lua
@@ -1,5 +1,30 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+
+-- Indent and unindent highlighted blocks
 vim.keymap.set('v', '<S-TAB>', '<gv')
 vim.keymap.set('v', '<TAB>', '>gv')
+
+-- Move highlighted blocks
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+
+-- Center cursor vertically while <C-d> and <C-u>
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+
+-- Paste and keep the previously yanked content instead of what just got deleted
+vim.keymap.set('x', '<leader>p', [["_dP]])
+
+-- Yank into system clipboard
+vim.keymap.set({'n', 'v'}, '<leader>y', [["+y]])
+vim.keymap.set('n', '<leader>Y', [["+Y]])
+
+-- Delete into void register
+vim.keymap.set({'n', 'v'}, '<leader>d', [["_d]])
+
+-- Don't do anything when pressing Q in normal mode
+vim.keymap.set('n', 'Q', '<nop>')
+
