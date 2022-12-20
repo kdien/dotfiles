5396843d5bf6a024589ffebfd39c79cf0f069b27
Khoa Dien <khoa.dien@bounteous.com>
2022-12-20 18:30:47 -0400

    Update neovim diagnostic format

--- neovim/after/plugin/lsp.lua
+++ neovim/after/plugin/lsp.lua
@@ -74,7 +74,31 @@ end)
 
 lsp.setup()
 
+local sev_text = function(diagnostic)
+  if diagnostic.severity == vim.diagnostic.severity.ERROR then
+    return 'ERROR'
+  elseif diagnostic.severity == vim.diagnostic.severity.WARN then
+    return 'WARN'
+  elseif diagnostic.severity == vim.diagnostic.severity.INFO then
+    return 'INFO'
+  elseif diagnostic.severity == vim.diagnostic.severity.HINT then
+    return 'HINT'
+  end
+end
 vim.diagnostic.config({
   virtual_text = false,
+  signs = false,
+  float = {
+    header = '',
+    border = 'single',
+    format = function(diagnostic)
+      return string.format(
+        '%s - %s: %s',
+        sev_text(diagnostic),
+        diagnostic.source,
+        diagnostic.message
+      )
+    end
+  }
 })
 
