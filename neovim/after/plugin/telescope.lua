f2255083c7fb118224d2b1c184d595f8864774d7
Khoa Dien <khoa.dien@bounteous.com>
2022-12-20 12:01:23 -0400

    Add telescope keymaps for string searching

--- neovim/after/plugin/telescope.lua
+++ neovim/after/plugin/telescope.lua
@@ -1,3 +1,5 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>sf', builtin.find_files, {})
+vim.keymap.set('n', '<leader>ss', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>st', builtin.grep_string, {})
 
