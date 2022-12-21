4a41f84408011b8087be4635070a7136cf914c13
Khoa Dien <khoa.dien@bounteous.com>
2022-12-21 12:40:00 -0400

    Fix neovim config for Ansible LSP

--- neovim/after/plugin/ansible.lua
+++ neovim/after/plugin/ansible.lua
@@ -1,2 +1,8 @@
-vim.keymap.set('n', '<leader>ab', ':set filetype=yaml.ansible<CR>:set filetype=ansible syntax=yaml.ansible<CR>')
+local set_ansible = function()
+  vim.cmd('LspStop')
+  vim.opt.filetype = 'ansible'
+  vim.opt.syntax = 'yaml.ansible'
+  vim.cmd('LspStart ansiblels')
+end
+vim.keymap.set('n', '<leader>ab', set_ansible)
 
