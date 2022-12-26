72a086749f03f1c3e0828119b805cd8fc65ab0f4
Khoa Dien <khoa.dien@hotmail.com>
2022-12-26 10:25:56 -0400

    Add neovim autocmd to remove textwidth restriction for gitcommit

--- neovim/lua/main/autocmd.lua
+++ neovim/lua/main/autocmd.lua
@@ -3,6 +3,11 @@ vim.api.nvim_create_autocmd('FileType', {
   command = 'setlocal tabstop=2 softtabstop=2 shiftwidth=2'
 })
 
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'gitcommit',
+  command = 'set textwidth=0'
+})
+
 vim.api.nvim_create_autocmd({'BufNewFile', 'BufRead'}, {
   pattern = vim.fn.expand '$HOME/.ssh/config.d/*',
   command = 'set filetype=sshconfig'
