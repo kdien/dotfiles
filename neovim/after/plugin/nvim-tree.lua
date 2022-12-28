905aba92d97252118f3248a0e678f7bf99db3a11
Khoa Dien <khoa.dien@hotmail.com>
2022-12-28 18:35:56 -0400

    Add `update_focused_file` config for nvim-tree

--- neovim/after/plugin/nvim-tree.lua
+++ neovim/after/plugin/nvim-tree.lua
@@ -7,7 +7,13 @@ require('nvim-tree').setup({
   },
   filters = {
     dotfiles = false
-  }
+  },
+  update_focused_file = {
+    enable = true,
+    debounce_delay = 15,
+    update_root = false,
+    ignore_list = {},
+  },
 })
 
 vim.keymap.set({'n', 'v'}, '<leader>tt', vim.cmd.NvimTreeToggle)
