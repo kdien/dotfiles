95d7c8847ee51c5e41635acb95e67251b27d3478
Khoa Dien <khoa.dien@hotmail.com>
2022-12-30 00:28:56 -0400

    Update working config for nvim-jdtls

--- neovim/ftplugin/java.lua
+++ neovim/ftplugin/java.lua
@@ -1,15 +1,134 @@
 local jdtls = require('jdtls')
-local home = os.getenv('HOME')
-local java_home = os.getenv('JAVA_HOME')
-local root_dir = require('jdtls.setup').find_root({'.git', 'mvnw', 'gradlew'})
-local mason_packages_root = home .. '/.local/share/nvim/mason/packages'
-local workspace_dir = home .. '/.local/share/eclipse/' .. vim.fn.fnamemodify(root_dir, ':p:h:t')
-local config = {}
 
-config.settings = {
-  java = {
+local home = os.getenv('HOME')
+local mason_packages_dir = home .. '/.local/share/nvim/mason/packages'
+
+if vim.fn.has 'mac' == 1 then
+  config_os = 'mac'
+elseif vim.fn.has 'unix' == 1 then
+  config_os = 'linux'
+else
+  print 'Unsupported system'
+end
+
+-- Find root of project
+local root_markers = { '.git', 'mvnw', 'gradlew', 'pom.xml', 'build.gradle' }
+local root_dir = require('jdtls.setup').find_root(root_markers)
+if root_dir == '' then
+  return
+end
+
+local extendedClientCapabilities = jdtls.extendedClientCapabilities
+extendedClientCapabilities.resolveAdditionalTextEditsSupport = true
+
+local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
+local workspace_dir = home .. '/eclipse-workspace/' .. project_name
+
+JAVA_DAP_ACTIVE = true
+local bundles = {}
+
+if JAVA_DAP_ACTIVE then
+  vim.list_extend(bundles, vim.split(vim.fn.glob(home .. '/.config/nvim/vscode-java-test/server/*.jar'), '\n'))
+  vim.list_extend(
+    bundles,
+    vim.split(
+      vim.fn.glob(
+        home .. '/.config/nvim/java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-*.jar'
+      ),
+      '\n'
+    )
+  )
+end
+
+-- See `:help vim.lsp.start_client` for an overview of the supported `config` options.
+local config = {
+  -- The command that starts the language server
+  -- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
+  cmd = {
+
+    'java', -- or '/path/to/java11_or_newer/bin/java'
+    -- depends on if `java` is in your $PATH env variable and if it points to the right version.
+
+    '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+    '-Dosgi.bundles.defaultStartLevel=4',
+    '-Declipse.product=org.eclipse.jdt.ls.core.product',
+    '-Dlog.protocol=true',
+    '-Dlog.level=ALL',
+    '-javaagent:' .. mason_packages_dir .. '/jdtls/lombok.jar',
+    '-Xms1g',
+    '--add-modules=ALL-SYSTEM',
+    '--add-opens',
+    'java.base/java.util=ALL-UNNAMED',
+    '--add-opens',
+    'java.base/java.lang=ALL-UNNAMED',
+
+    '-jar',
+    vim.fn.glob(mason_packages_dir .. '/jdtls/plugins/org.eclipse.equinox.launcher_*.jar'),
+
+    '-configuration',
+    mason_packages_dir .. '/jdtls/config_' .. config_os,
+
+    '-data',
+    workspace_dir,
+  },
+
+  -- This is the default if not provided, you can remove it. Or adjust as needed.
+  -- One dedicated LSP server & client will be started per unique root_dir
+  root_dir = root_dir,
+
+  -- Here you can configure eclipse.jdt.ls specific settings
+  -- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
+  -- or https://github.com/redhat-developer/vscode-java#supported-vs-code-settings
+  -- for a list of options
+  settings = {
+    java = {
+      -- jdt = {
+      --   ls = {
+      --     vmargs = "-XX:+UseParallelGC -XX:GCTimeRatio=4 -XX:AdaptiveSizePolicyWeight=90 -Dsun.zip.disableMemoryMapping=true -Xmx1G -Xms100m"
+      --   }
+      -- },
+      eclipse = {
+        downloadSources = true,
+      },
+      configuration = {
+        updateBuildConfiguration = 'interactive',
+        runtimes = {
+          {
+            name = 'JavaSE-1.8',
+            path = '/usr/lib/jvm/java-1.8.0-openjdk/'
+          },
+          {
+            name = 'JavaSE-11',
+            path = '/usr/lib/jvm/java-11-openjdk/'
+          },
+          {
+            name = 'JavaSE-17',
+            path = '/usr/lib/jvm/java-17-openjdk/'
+          },
+        }
+      },
+      maven = {
+        downloadSources = true,
+      },
+      implementationsCodeLens = {
+        enabled = true,
+      },
+      referencesCodeLens = {
+        enabled = true,
+      },
+      references = {
+        includeDecompiledSources = true,
+      },
+      inlayHints = {
+        parameterNames = {
+          enabled = 'all', -- literals, all, none
+        },
+      },
+      format = {
+        enabled = false,
+      },
+    },
     signatureHelp = { enabled = true },
-    contentProvider = { preferred = 'fernflower' },
     completion = {
       favoriteStaticMembers = {
         'org.hamcrest.MatcherAssert.assertThat',
@@ -18,16 +137,11 @@ config.settings = {
         'org.junit.jupiter.api.Assertions.*',
         'java.util.Objects.requireNonNull',
         'java.util.Objects.requireNonNullElse',
-        'org.mockito.Mockito.*'
-      },
-      filteredTypes = {
-        'com.sun.*',
-        'io.micrometer.shaded.*',
-        'java.awt.*',
-        'jdk.*',
-        'sun.*',
+        'org.mockito.Mockito.*',
       },
     },
+    contentProvider = { preferred = 'fernflower' },
+    extendedClientCapabilities = extendedClientCapabilities,
     sources = {
       organizeImports = {
         starThreshold = 9999,
@@ -36,115 +150,60 @@ config.settings = {
     },
     codeGeneration = {
       toString = {
-        template = '${object.className}{${member.name()}=${member.value}, ${otherMembers}}'
-      },
-      hashCodeEquals = {
-        useJava7Objects = true,
+        template = '${object.className}{${member.name()}=${member.value}, ${otherMembers}}',
       },
       useBlocks = true,
     },
-    configuration = {
-      runtimes = {
-        {
-          name = 'JavaSE-1.8',
-          path = '/usr/lib/jvm/java-1.8.0-openjdk/',
-        },
-        {
-          name = 'JavaSE-11',
-          path = '/usr/lib/jvm/java-11-openjdk/',
-        },
-        {
-          name = 'JavaSE-17',
-          path = '/usr/lib/jvm/java-17-openjdk/',
-        },
-      }
-    },
+  },
+
+  flags = {
+    allow_incremental_sync = true,
+  },
+
+  -- Language server `initializationOptions`
+  -- You need to extend the `bundles` with paths to jar files
+  -- if you want to use additional eclipse.jdt.ls plugins.
+  --
+  -- See https://github.com/mfussenegger/nvim-jdtls#java-debug-installation
+  --
+  -- If you don't plan on using the debugger or other eclipse.jdt.ls plugins you can remove this
+  init_options = {
+    bundles = bundles,
   },
 }
 
-config.cmd = {
-  java_home .. '/bin/java',
-  '-Declipse.application=org.eclipse.jdt.ls.core.id1',
-  '-Dosgi.bundles.defaultStartLevel=4',
-  '-Declipse.product=org.eclipse.jdt.ls.core.product',
-  '-Dlog.protocol=true',
-  '-Dlog.level=ALL',
-  '-Xmx4g',
-  '--add-modules=ALL-SYSTEM',
-  '--add-opens', 'java.base/java.util=ALL-UNNAMED',
-  '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
-  '-jar', vim.fn.glob(mason_packages_root .. '/jdtls/plugins/org.eclipse.equinox.launcher_*.jar'),
-  '-configuration', mason_packages_root .. '/jdtls/config_linux',
-  '-data', workspace_dir,
-}
-
-config.on_attach = function(client, bufnr)
-  -- jdtls.setup_dap({ hotcodereplace = 'auto' })
-  jdtls.setup.add_commands()
-
-  local opts = { silent = true, buffer = bufnr }
-  vim.keymap.set('n', '<leader>oi>', jdtls.organize_imports, opts)
-  vim.keymap.set('n', '<leader>dt', jdtls.test_class, opts)
-  vim.keymap.set('n', '<leader>dm', jdtls.test_nearest_method, opts)
-  vim.keymap.set('n', '<leader>xv', jdtls.extract_variable, opts)
-  vim.keymap.set('v', '<leader>xm', vim.cmd("lua require('jdtls').extract_method(true)"), opts)
-  vim.keymap.set('n', '<leader>xc', jdtls.extract_constant, opts)
-  vim.keymap.set('n', '<leader>rui', function()
-    vim.diagnostic.setqflist { severity = vim.diagnostic.severity.WARN }
-    vim.cmd('packadd cfilter')
-    vim.cmd('Cfilter /main/')
-    vim.cmd('Cfilter /The import/')
-    vim.cmd('cdo normal dd')
-    vim.cmd('cclose')
-    vim.cmd('wa')
-  end)
-end
-
-local jar_patterns = {
-  mason_packages_root .. '/java-debug-adapter/extension/server/com.microsoft.java.debug.plugin-*.jar',
-  mason_packages_root .. '/java-test/extension/server/com.microsoft.java.test.plugin-*.jar',
-  mason_packages_root .. '/java-test/extension/server/com.microsoft.java.test.runner-*.jar',
-}
--- npm install broke for me: https://github.com/npm/cli/issues/2508
--- So gather the required jars manually; this is based on the gulpfile.js in the vscode-java-test repo
-local plugin_path = mason_packages_root .. '/java-test/extension/server/'
-local bundle_list = vim.tbl_map(
-  function(x) return require('jdtls.path').join(plugin_path, x) end,
-  {
-    'org.eclipse.jdt.junit4.runtime_*.jar',
-    'org.eclipse.jdt.junit5.runtime_*.jar',
-    'org.junit.jupiter.api*.jar',
-    'org.junit.jupiter.engine*.jar',
-    'org.junit.jupiter.migrationsupport*.jar',
-    'org.junit.jupiter.params*.jar',
-    'org.junit.vintage.engine*.jar',
-    'org.opentest4j*.jar',
-    'org.junit.platform.commons*.jar',
-    'org.junit.platform.engine*.jar',
-    'org.junit.platform.launcher*.jar',
-    'org.junit.platform.runner*.jar',
-    'org.junit.platform.suite.api*.jar',
-    'org.apiguardian*.jar'
-  }
-)
-vim.list_extend(jar_patterns, bundle_list)
-local bundles = {}
-for _, jar_pattern in ipairs(jar_patterns) do
-  for _, bundle in ipairs(vim.split(vim.fn.glob(home .. jar_pattern), '\n')) do
-    if not vim.endswith(bundle, 'com.microsoft.java.test.runner-jar-with-dependencies.jar')
-      and not vim.endswith(bundle, 'com.microsoft.java.test.runner.jar') then
-      table.insert(bundles, bundle)
-    end
-  end
-end
-
-local extendedClientCapabilities = jdtls.extendedClientCapabilities
-extendedClientCapabilities.resolveAdditionalTextEditsSupport = true
-
-config.init_options = {
-  bundles = bundles,
-  extendedClientCapabilities = extendedClientCapabilities,
-}
-
+-- This starts a new client & server,
+-- or attaches to an existing client & server depending on the `root_dir`.
 jdtls.start_or_attach(config)
+
+--[[ if JAVA_DAP_ACTIVE then
+  require('jdtls').setup_dap { hotcodereplace = 'auto' }
+  require('jdtls.dap').setup_dap_main_class_configs()
+end ]]
+
+vim.cmd "command! -buffer -nargs=? -complete=custom,v:lua.require'jdtls'._complete_compile JdtCompile lua require('jdtls').compile(<f-args>)"
+vim.cmd "command! -buffer -nargs=? -complete=custom,v:lua.require'jdtls'._complete_set_runtime JdtSetRuntime lua require('jdtls').set_runtime(<f-args>)"
+vim.cmd "command! -buffer JdtUpdateConfig lua require('jdtls').update_project_config()"
+-- vim.cmd "command! -buffer JdtJol lua require('jdtls').jol()"
+vim.cmd "command! -buffer JdtBytecode lua require('jdtls').javap()"
+-- vim.cmd "command! -buffer JdtJshell lua require('jdtls').jshell()"
+
+local opts = { silent = true, buffer = nil }
+vim.keymap.set('n', '<leader>oi', jdtls.organize_imports, opts)
+vim.keymap.set('n', '<leader>dt', jdtls.test_class, opts)
+vim.keymap.set('n', '<leader>dm', jdtls.test_nearest_method, opts)
+vim.keymap.set('n', '<leader>xv', jdtls.extract_variable, opts)
+vim.keymap.set('v', '<leader>xm', vim.cmd("lua require('jdtls').extract_method(true)"), opts)
+vim.keymap.set('n', '<leader>xc', jdtls.extract_constant, opts)
+
+-- Remove unused imports
+vim.keymap.set('n', '<leader>rui', function()
+  vim.diagnostic.setqflist { severity = vim.diagnostic.severity.WARN }
+  vim.cmd('packadd cfilter')
+  vim.cmd('Cfilter /main/')
+  vim.cmd('Cfilter /The import/')
+  vim.cmd('cdo normal dd')
+  vim.cmd('cclose')
+  vim.cmd('wa')
+end, opts)
 
