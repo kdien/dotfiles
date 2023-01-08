d400df6ddd95d1063ff00c6cf240d5823015d931
Khoa Dien <khoa.dien@hotmail.com>
2023-01-07 21:53:04 -0400

    Update telescope configs
    - Map <Esc> to close telescope in insert mode
    - Add mappings to search help docs and show effective keymaps

--- neovim/after/plugin/telescope.lua
+++ neovim/after/plugin/telescope.lua
@@ -1,5 +1,15 @@
+local telescope = require('telescope')
+local actions = require('telescope.actions')
 local grep_args = { '--hidden' }
-require('telescope').setup {
+
+telescope.setup {
+  defaults = {
+    mappings = {
+      i = {
+        ['<esc>'] = actions.close
+      },
+    },
+  },
   pickers = {
     find_files = {
       hidden = true
@@ -21,4 +31,6 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>sf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>ss', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>st', builtin.grep_string, {})
+vim.keymap.set('n', '<leader>sh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>km', builtin.keymaps, {})
 
