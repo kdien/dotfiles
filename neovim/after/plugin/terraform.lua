f3c8fa802392f365e27916408016a8c8a9815ce7
Khoa Dien <khoa.dien@hotmail.com>
2023-01-09 00:08:16 -0400

    Update terraform keymaps in neovim
    - Create separate terraform config file
    - Add keymap to yank current terraform resource

---
+++ neovim/after/plugin/terraform.lua
@@ -0,0 +1,3 @@
+vim.keymap.set('n', '<leader>yrs', 'ma<Cmd>?^resource<CR>yy`a<Cmd>delmarks a<CR>')
+vim.keymap.set('n', '<leader>tft', '<Cmd>%s/^resource "\\(.*\\)" "\\(.*\\)".*/-target \\1.\\2 /<CR>')
+
