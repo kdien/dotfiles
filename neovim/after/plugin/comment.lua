2503af0b3dac83eeeec14da1055a38e9f1a93e44
Khoa Dien <khoa.dien@hotmail.com>
2023-01-19 23:45:27 -0400

    Update Comment.nvim keymaps to use `g` instead of `<leader`

--- neovim/after/plugin/comment.lua
+++ neovim/after/plugin/comment.lua
@@ -1,25 +1,25 @@
 require('Comment').setup {
   toggler = {
     ---Line-comment toggle keymap
-    line = '<leader>cc',
+    line = 'gcc',
     ---Block-comment toggle keymap
-    block = '<leader>bc',
+    block = 'gbc',
   },
   ---LHS of operator-pending mappings in NORMAL and VISUAL mode
   opleader = {
     ---Line-comment keymap
-    line = '<leader>c',
+    line = 'gc',
     ---Block-comment keymap
-    block = '<leader>b',
+    block = 'gb',
   },
   ---LHS of extra mappings
   extra = {
     ---Add comment on the line above
-    above = '<leader>cO',
+    above = 'gcO',
     ---Add comment on the line below
-    below = '<leader>co',
+    below = 'gco',
     ---Add comment at the end of line
-    eol = '<leader>cA',
+    eol = 'gcA',
   },
 }
 
