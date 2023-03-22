0248c1a5afd9352537e460b4b06f11e2ef6b13e0
Khoa Dien <khoa.dien@hotmail.com>
2023-03-21 23:56:00 -0300

    Add script to set up neovim dependencies for Java

---
+++ neovim/dependencies/setup.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+rm -rf java-debug
+git clone https://github.com/microsoft/java-debug.git
+cd java-debug || return
+mvn clean install
+cd ..
+
+rm -rf vscode-java-test
+git clone https://github.com/microsoft/vscode-java-test.git
+cd vscode-java-test || return
+npm install
+npm run build-plugin
+cd ..
+
+rm -rf eclipse.jdt.ls
+git clone https://github.com/eclipse/eclipse.jdt.ls.git
+cd eclipse.jdt.ls || return
+version=$(git describe --tags | cut -d '-' -f 1 | cut -d 'v' -f 2)
+filename=$(curl -sSL "https://download.eclipse.org/jdtls/milestones/$version/latest.txt")
+cd ..
+echo "Downloading $filename"
+curl -sSLO "https://www.eclipse.org/downloads/download.php?file=/jdtls/milestones/$version/$filename"
+rm -rf jdtls
+mkdir -p jdtls
+echo "Extracting $filename"
+tar xf "$filename" -C jdtls
+echo 'Cleaning up'
+rm -rf eclipse.jdt.ls
+rm -f "$filename"
+echo 'Done!'
+
