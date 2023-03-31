12630caf19774f14c74b57dfc62803ac4cb85d4b
Khoa Dien <khoa.dien@hotmail.com>
2023-03-31 15:19:04 -0300

    Add keymap to open tmux split window

--- neovim/lua/main/keymaps.lua
+++ neovim/lua/main/keymaps.lua
@@ -36,8 +36,9 @@ vim.keymap.set({'n', 'v'}, '<leader>d', [["_d]])
 -- Don't do anything when pressing Q in normal mode
 vim.keymap.set('n', 'Q', '<nop>')
 
--- Open new tmux window to the current working directory
+-- Open new tmux (split-)window to the current working directory
 vim.keymap.set('n', '<leader>term', '<Cmd>!tmux new-window -c ' .. vim.fn.getcwd() .. '<CR><CR>')
+vim.keymap.set('n', '<leader>sterm', '<Cmd>!tmux split-window -c ' .. vim.fn.getcwd() .. '<CR><CR>')
 
 -- Move between buffers
 vim.keymap.set('n', '<leader>.', '<Cmd>bnext<CR>')
