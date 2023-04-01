69879a5d602c7acf62aa30365e3ef1ed182703a0
Khoa Dien <khoa.dien@hotmail.com>
2023-04-01 13:55:01 -0300

    Add condition to only load `impatient` if it exists

--- neovim/lua/main/init.lua
+++ neovim/lua/main/init.lua
@@ -1,4 +1,7 @@
-require('impatient')
+local impatient_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/impatient.nvim'
+if vim.fn.empty(vim.fn.glob(impatient_path)) == 0 then
+  require('impatient')
+end
 
 require('main.opt')
 require('main.keymaps')
