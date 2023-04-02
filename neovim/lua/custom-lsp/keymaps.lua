c427ccd05aaaf2c452b95bc261538e79ccf35593
Khoa Dien <khoa.dien@hotmail.com>
2023-04-02 14:26:10 -0300

    Update LSP keymaps to use telescope builtin functions where available

--- neovim/lua/custom-lsp/keymaps.lua
+++ neovim/lua/custom-lsp/keymaps.lua
@@ -1,11 +1,13 @@
-vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
+local tlsp_builtin = require('telescope.builtin')
+
+vim.keymap.set('n', 'gd', tlsp_builtin.lsp_definitions)
 vim.keymap.set('n', 'K', vim.lsp.buf.hover)
-vim.keymap.set('n', '<leader>vws', vim.lsp.buf.workspace_symbol)
+vim.keymap.set('n', '<leader>vws', tlsp_builtin.lsp_workspace_symbols)
 vim.keymap.set('n', '<leader>vd', vim.diagnostic.open_float)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
 vim.keymap.set('n', '<leader>vca', vim.lsp.buf.code_action)
-vim.keymap.set('n', '<leader>vrr', vim.lsp.buf.references)
+vim.keymap.set('n', '<leader>vrr', tlsp_builtin.lsp_references)
 vim.keymap.set('n', '<leader>vrn', vim.lsp.buf.rename)
 vim.keymap.set('i', '<C-h>', vim.lsp.buf.signature_help)
 
