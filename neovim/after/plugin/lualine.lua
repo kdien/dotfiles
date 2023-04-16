2570c618c62a60de36bfa54872ed7696c6f68c14
Khoa Dien <khoa.dien@hotmail.com>
2023-04-15 23:19:38 -0300

    Update lualine.nvim config
    - Show full path for filename
    - Show `LF` and `CRLF` for line ending format instead of icons
    - Reformat lualine config code for better readability

--- neovim/after/plugin/lualine.lua
+++ neovim/after/plugin/lualine.lua
@@ -1,11 +1,34 @@
 require('lualine').setup {
   sections = {
-    lualine_a = { 'mode' },
-    lualine_b = { 'branch', 'diff', 'diagnostics' },
-    lualine_c = { 'filename' },
-    lualine_x = { 'filetype', 'fileformat', 'encoding' },
-    lualine_y = { 'progress' },
-    lualine_z = { 'location' }
+    lualine_a = {
+      'mode'
+    },
+    lualine_b = {
+      'branch', 'diff', 'diagnostics'
+    },
+    lualine_c = {
+      {
+        'filename',
+        path = 3
+      },
+    },
+    lualine_x = {
+      'filetype',
+      {
+        'fileformat',
+        symbols = {
+          unix = 'LF',
+          dos = 'CRLF'
+        }
+      },
+      'encoding'
+    },
+    lualine_y = {
+      'progress'
+    },
+    lualine_z = {
+      'location'
+    }
   },
 }
 
