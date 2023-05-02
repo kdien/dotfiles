5925927d249b3ad806befd98e2af956016e70276
Khoa Dien <khoa.dien@hotmail.com>
2023-05-02 11:41:06 -0300

    Tweak neovim config for ansible language server

--- neovim/after/plugin/ansible.lua
+++ neovim/after/plugin/ansible.lua
@@ -1,8 +1,9 @@
 local set_ansible = function()
   vim.cmd('LspStop')
+  vim.opt.filetype = 'yaml.ansible'
+  vim.cmd('LspStart ansiblels')
   vim.opt.filetype = 'ansible'
   vim.opt.syntax = 'yaml.ansible'
-  vim.cmd('LspStart ansiblels')
 end
 vim.keymap.set('n', '<leader>ab', set_ansible)
 
