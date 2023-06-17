e94b468292dfdc77988fd4c7c62b5c9a1f9eb0a8
Khoa Dien <khoa.dien@hotmail.com>
2023-06-17 17:19:06 -0300

    Move terraform config to ftplugin directory
    - Update format keymap to use `silent` flag
    - Add autocmd to format on save

--- neovim/ftplugin/terraform.lua
+++ neovim/ftplugin/terraform.lua
@@ -1,4 +1,8 @@
-vim.keymap.set('n', '<leader>tfm', '<Cmd>!terraform fmt %<CR><CR>')
+vim.keymap.set('n', '<leader>tfm', '<Cmd>silent !terraform fmt %<CR>')
 vim.keymap.set('n', '<leader>yrs', 'ma<Cmd>?^resource<CR>yy`a<Cmd>delmarks a<CR>')
 vim.keymap.set('n', '<leader>tft', '<Cmd>%s/^resource "\\(.*\\)" "\\(.*\\)".*/-target \\1.\\2 /<CR>')
 
+vim.api.nvim_create_autocmd('BufWritePost', {
+  pattern = '*',
+  command = 'silent !terraform fmt %'
+})
