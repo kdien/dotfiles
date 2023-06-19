43b92caeacd39977ea07ad1f50d5bb837a9f4dc1
Khoa Dien <khoa.dien@hotmail.com>
2023-06-18 22:50:14 -0300

    Pass function directly into neovim ansible keymap

--- neovim/after/plugin/ansible.lua
+++ neovim/after/plugin/ansible.lua
@@ -1,8 +1,5 @@
-local set_ansible = function()
+vim.keymap.set('n', '<leader>ab', function()
   vim.lsp.stop_client(vim.lsp.get_active_clients({name = 'yamlls'}))
   vim.opt.filetype = 'ansible'
   vim.opt.syntax = 'yaml.ansible'
-end
-
-vim.keymap.set('n', '<leader>ab', set_ansible)
-
+end)
