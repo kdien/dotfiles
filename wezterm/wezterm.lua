99b3bcf7b5ad59425edcc9d393076268c784f903
Khoa Dien <khoa.dien@hotmail.com>
2023-07-02 14:09:42 -0300

    Update WezTerm config to support macOS

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,6 +6,12 @@ wezterm.on('gui-startup', function()
   window:gui_window():maximize()
 end)
 
+local platform = 'linux'
+local home = require('os').getenv('HOME') or ''
+if string.match(home, '^/Users') then
+  platform = 'mac'
+end
+
 local config = {}
 
 config.automatically_reload_config = true
@@ -13,28 +19,31 @@ config.automatically_reload_config = true
 config.window_decorations = 'RESIZE'
 config.enable_tab_bar = false
 config.enable_scroll_bar = false
+config.window_close_confirmation = 'NeverPrompt'
 
 config.colors = require('colors')
 
 config.default_cursor_style = 'SteadyBlock'
 
-config.font = wezterm.font_with_fallback({
-  'SFMono',
-  'Menlo',
-  'Consolas',
-  'monospace',
-})
+local fonts = {}
+table.insert(fonts, platform == 'mac' and 'SF Mono' or 'SFMono')
+table.insert(fonts, 'Menlo')
+table.insert(fonts, 'Consolas')
+table.insert(fonts, 'monospace')
 
-config.font_size = 11.5
+config.font = wezterm.font_with_fallback(fonts)
+
+config.font_size = platform == 'mac' and 15.0 or 11.5
 
 config.line_height = 1.15
 
-config.window_padding = {
-  left = 4,
-  right = 4,
-  top = 4,
-  bottom = 4,
-}
+local padding = {}
+padding['left'] = platform == 'mac' and 8 or 4
+padding['right'] = platform == 'mac' and 8 or 4
+padding['top'] = platform == 'mac' and 8 or 4
+padding['bottom'] = platform == 'mac' and 8 or 4
+
+config.window_padding = padding
 
 config.selection_word_boundary = ' \t\n;,\'"'
 
