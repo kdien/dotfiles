c5d59980855b009dc3c83a2d05568b0d75b22b41
Khoa Dien <khoa.dien@hotmail.com>
2023-07-09 13:33:46 -0300

    Remove autocmd from `packer` config
    - It never worked since it was copied from `kickstart.nvim` where the whole
    config is in `init.lua`
    - Running `PackerSync` on every save creates annoying delays

--- nvim/lua/main/packer.lua
+++ nvim/lua/main/packer.lua
@@ -96,12 +96,3 @@ require('packer').startup(function(use)
     require('packer').sync()
   end
 end)
-
--- Automatically source and re-compile packer whenever you save this init.lua
-local packer_group = vim.api.nvim_create_augroup('Packer', { clear = true })
-vim.api.nvim_create_autocmd('BufWritePost', {
-  command = 'source <afile> | PackerCompile',
-  group = packer_group,
-  pattern = vim.fn.expand '$MYVIMRC',
-})
-
