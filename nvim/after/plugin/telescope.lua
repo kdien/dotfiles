d7d3056ed6b182ea6a5a53eb6eabe373aa960a42
Khoa Dien <khoa.dien@hotmail.com>
2023-07-24 16:37:55 -0300

    Update nvim telescope config
    - Remove custom option for find_files picker
    - Add keymap for searching git_files

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -15,9 +15,6 @@ telescope.setup {
     },
   },
   pickers = {
-    find_files = {
-      find_command = { 'fd', '--type', 'f', '--hidden', '--exclude', '.git' }
-    },
     live_grep = {
       additional_args = function(opts)
         return grep_args
@@ -34,6 +31,7 @@ telescope.setup {
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>sr', builtin.resume, {})
 vim.keymap.set('n', '<leader>sf', builtin.find_files, {})
+vim.keymap.set('n', '<leader>sg', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ss', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>st', builtin.grep_string, {})
 vim.keymap.set('n', '<leader>sh', builtin.help_tags, {})
