fa30262365808745ab2ba8254dde10d9fb999e6b
Khoa Dien <khoa.dien@hotmail.com>
2023-07-24 20:42:47 -0300

    Improve telescope nvim config
    - Add global option for `find_files` to always include hidden files
    - Add keymap for custom `find_files` to search from git root directory and
    include files ignored by git
    - Add option for `git_files` to include untracked files

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -15,6 +15,9 @@ telescope.setup {
     },
   },
   pickers = {
+    find_files = {
+      hidden = true,
+    },
     live_grep = {
       additional_args = function(opts)
         return grep_args
@@ -31,13 +34,23 @@ telescope.setup {
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>sr', builtin.resume, {})
 vim.keymap.set('n', '<leader>sf', builtin.find_files, {})
-vim.keymap.set('n', '<leader>sg', builtin.git_files, {})
+
+vim.keymap.set('n', '<leader>sa', function()
+  local git_root = vim.fn.system("git rev-parse --show-toplevel | tr -d '\n'")
+  builtin.find_files({ cwd = git_root, no_ignore = true })
+end, {})
+
+vim.keymap.set('n', '<leader>sg', function()
+  builtin.git_files({ show_untracked = true })
+end, {})
+
 vim.keymap.set('n', '<leader>ss', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>st', builtin.grep_string, {})
 vim.keymap.set('n', '<leader>sh', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>km', builtin.keymaps, {})
-vim.keymap.set('n', '<leader>ls', function()
-  builtin.buffers({sort_lastused = true})
-end, {})
-vim.keymap.set('n', '<leader>di', builtin.diagnostics, {})
 
+vim.keymap.set('n', '<leader>ls', function()
+  builtin.buffers({ sort_lastused = true })
+end, {})
+
+vim.keymap.set('n', '<leader>di', builtin.diagnostics, {})
