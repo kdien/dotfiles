075b13e5666f081bbb62a7ffab5001ca7d9bdee3
Khoa Dien <khoa.dien@hotmail.com>
2023-08-03 11:19:58 -0300

    Add firefox customization for macOS

---
+++ firefox-macos/user.js
@@ -0,0 +1,6 @@
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("browser.compactmode.show", true);
+user_pref("svg.context-properties.content.enabled", true);
+user_pref("layout.css.color-mix.enabled", true);
+user_pref("full-screen-api.macos-native-full-screen", true);
+user_pref("full-screen-api.warning.timeout", 1000);
