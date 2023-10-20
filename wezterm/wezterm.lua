d23bb409056fe3044473be6969ed201650d1346a
Khoa Dien <khoa.dien@hotmail.com>
2023-10-20 15:37:10 -0300

    Add conditions for WezTerm font config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -51,26 +51,44 @@ config.colors = require('colors')
 
 config.default_cursor_style = 'SteadyBlock'
 
+local font_family = 'MesloLGM Nerd Font'
+
 config.font = wezterm.font({
-  family = 'MesloLGM Nerd Font',
-  weight = 'Regular'
+  family = font_family,
+  weight = string.match(font_family, 'JetBrains') and 'Light' or 'Regular'
 })
 
 config.font_rules = {
   {
     intensity = 'Bold',
     font = wezterm.font({
-      family = 'MesloLGM Nerd Font',
-      weight = 'Bold'
+      family = font_family,
+      weight = (string.match(font_family, 'Fira') or string.match(font_family, 'JetBrains')) and 'Medium' or 'Bold'
     })
   }
 }
 
-config.font_size = platform == 'mac' and 15.0 or 11.5
--- config.line_height = 1.15
+if string.match(font_family, 'Fira') then
+  config.line_height = 1.1
+elseif string.match(font_family, 'Bitstr') then
+  config.line_height = 1.15
+end
+
+if string.match(font_family, 'Fira ?Code') then
+  config.font_size = platform == 'mac' and 14.5 or 11.0
+else
+  config.font_size = platform == 'mac' and 15.0 or 11.5
+end
+
 config.bold_brightens_ansi_colors = false
 config.freetype_load_flags = 'NO_HINTING'
 config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' } -- disable ligatures
+
+if string.match(font_family, 'Fira ?Code') then
+  table.insert(config.harfbuzz_features, 'zero')
+  table.insert(config.harfbuzz_features, 'ss03') -- &
+  table.insert(config.harfbuzz_features, 'ss05') -- @
+end
 
 local padding = {}
 padding['left'] = platform == 'mac' and 8 or 4
