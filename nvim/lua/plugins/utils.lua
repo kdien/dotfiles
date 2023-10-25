7c3e76e68c65d37612b807ae8fb62eedc0dbb83c
Khoa Dien <khoa.dien@hotmail.com>
2023-10-25 00:31:38 -0300

    Add vim-rest-console plugin

--- nvim/lua/plugins/utils.lua
+++ nvim/lua/plugins/utils.lua
@@ -22,5 +22,24 @@ return {
     config = function()
       require('Comment').setup()
     end
+  },
+
+  {
+    'diepm/vim-rest-console',
+    ft = 'rest',
+
+    config = function()
+      vim.g.vrc_allow_get_request_body = 1
+      vim.g.vrc_auto_format_response_enabled = 1
+
+      local vrc_curl_opts = {}
+      vrc_curl_opts['-sSL'] = ''
+      vim.g.vrc_curl_opts = vrc_curl_opts
+
+      vim.g.vrc_keepalt = 1
+      vim.g.vrc_response_default_content_type = 'application/json'
+      vim.g.vrc_set_default_mapping = 0
+      vim.keymap.set('n', '<leader>rq', '<Cmd>call VrcQuery()<CR>')
+    end
   }
 }
