1d01532fbd35ee7a8b1223698ecc6e30487a5650
Khoa Dien <khoa.dien@hotmail.com>
2023-10-25 17:04:40 -0300

    Simplify WezTerm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,16 +6,6 @@ wezterm.on('gui-startup', function()
   window:gui_window():maximize()
 end)
 
-wezterm.on('toggle-ligatures', function(window)
-  local overrides = window:get_config_overrides() or {}
-  if not overrides.harfbuzz_features then
-    overrides.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
-  else
-    overrides.harfbuzz_features = nil
-  end
-  window:set_config_overrides(overrides)
-end)
-
 local platform = 'linux'
 local home = require('os').getenv('HOME')
 if not home then
@@ -55,50 +45,24 @@ local font_family = 'MesloLGM Nerd Font'
 
 config.font = wezterm.font({
   family = font_family,
-  weight = string.match(font_family, 'JetBrains') and 'Light' or 'Regular'
+  weight = 'Regular'
 })
-
-local function get_bold_weight()
-  if string.match(font_family, 'Fira') or string.match(font_family, 'JetBrains') then
-    return 'Medium'
-  elseif string.match(font_family, 'Monaco') then
-    return 'Regular'
-  else
-    return 'Bold'
-  end
-end
 
 config.font_rules = {
   {
     intensity = 'Bold',
     font = wezterm.font({
       family = font_family,
-      weight = get_bold_weight()
+      weight = 'Bold'
     })
   }
 }
 
-if string.match(font_family, 'Fira') then
-  config.line_height = 1.1
-elseif string.match(font_family, 'Bitstr') then
-  config.line_height = 1.15
-end
-
-if string.match(font_family, 'Fira ?Code') then
-  config.font_size = platform == 'mac' and 14.5 or 11.0
-else
-  config.font_size = platform == 'mac' and 15.0 or 11.5
-end
-
+config.font_size = platform == 'mac' and 15.0 or 11.5
+-- config.line_height = 1.15
 config.bold_brightens_ansi_colors = false
 config.freetype_load_flags = 'NO_HINTING'
 config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' } -- disable ligatures
-
-if string.match(font_family, 'Fira ?Code') then
-  table.insert(config.harfbuzz_features, 'zero')
-  table.insert(config.harfbuzz_features, 'ss03') -- &
-  table.insert(config.harfbuzz_features, 'ss05') -- @
-end
 
 local padding = {}
 padding['left'] = platform == 'mac' and 8 or 4
@@ -109,13 +73,5 @@ padding['bottom'] = platform == 'mac' and 8 or 4
 config.window_padding = padding
 
 config.selection_word_boundary = ' \t\n;,\'"'
-
-config.keys = {
-  {
-    key = 'L',
-    mods = 'CTRL',
-    action = wezterm.action.EmitEvent('toggle-ligatures')
-  }
-}
 
 return config
