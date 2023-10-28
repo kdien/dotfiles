81744e400274c1e733948c697e0fa052eb1eb2e8
Khoa Dien <khoa.dien@hotmail.com>
2023-10-27 23:25:07 -0300

    Add `mason-nvim-dap` plugin

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -7,12 +7,12 @@ return {
     },
 
     keys = {
-      { '<F5>', "<Cmd>lua require('dap').continue()<CR>" },
-      { '<F10>', "<Cmd>lua require('dap').step_over()<CR>" },
-      { '<F11>', "<Cmd>lua require('dap').step_into()<CR>" },
-      { '<F12>', "<Cmd>lua require('dap').step_out()<CR>" },
-      { '<Leader>b', "<Cmd>lua require('dap').toggle_breakpoint()<CR>" },
-      { '<Leader>B', "<Cmd>lua require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>" },
+      { '<F5>',       "<Cmd>lua require('dap').continue()<CR>" },
+      { '<F10>',      "<Cmd>lua require('dap').step_over()<CR>" },
+      { '<F11>',      "<Cmd>lua require('dap').step_into()<CR>" },
+      { '<F12>',      "<Cmd>lua require('dap').step_out()<CR>" },
+      { '<Leader>b',  "<Cmd>lua require('dap').toggle_breakpoint()<CR>" },
+      { '<Leader>B',  "<Cmd>lua require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>" },
       { '<Leader>lp', "<Cmd>lua require('dap').set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>" },
       { '<Leader>dr', "<Cmd>lua require('dap').repl.open()<CR>" },
       { '<Leader>dl', "<Cmd>lua require('dap').run_last()<CR>" }
@@ -35,6 +35,24 @@ return {
       end
 
       require('nvim-dap-virtual-text').setup({})
+    end
+  },
+
+  {
+    'jay-babu/mason-nvim-dap.nvim',
+    event = { 'BufReadPre', 'BufNewFile' },
+
+    dependencies = {
+      'williamboman/mason.nvim',
+      'mfussenegger/nvim-dap'
+    },
+
+    config = function()
+      require('mason-nvim-dap').setup({
+        ensure_installed = {
+          'delve',
+        }
+      })
     end
   },
 
