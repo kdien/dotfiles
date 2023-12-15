6d8f1135a879599f22180153ab51c6c3003af490
Khoa Dien <khoa.dien@hotmail.com>
2023-12-15 17:39:45 -0400

    Use default separators for lualine

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -24,8 +24,8 @@ return {
     config = function()
       require('lualine').setup({
         options = {
-          component_separators = '|',
-          section_separators = '',
+          -- component_separators = '|',
+          -- section_separators = '',
           icons_enabled = false
         },
         sections = {
