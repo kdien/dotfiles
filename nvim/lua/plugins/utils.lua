c542e0ad57ca1159cc632d6b118281876e6273c0
Khoa Dien <khoa.dien@hotmail.com>
2024-03-06 13:28:09 -0400

    Add `mason-tool-installer.nvim`

--- nvim/lua/plugins/utils.lua
+++ nvim/lua/plugins/utils.lua
@@ -2,6 +2,29 @@ local event = { 'BufReadPre', 'BufNewFile' }
 
 return {
   {
+    'WhoIsSethDaniel/mason-tool-installer.nvim',
+    event = event,
+    dependencies = 'williamboman/mason.nvim',
+
+    config = function()
+      require('mason-tool-installer').setup({
+        ensure_installed = {
+          'black',
+          'editorconfig-checker',
+          'shellcheck',
+          'shfmt',
+          'stylua',
+        },
+
+        auto_update = false,
+        run_on_start = true,
+        start_delay = 3000,
+        debounce_hours = 5,
+      })
+    end
+  },
+
+  {
     'tpope/vim-fugitive',
     event = event
   },
