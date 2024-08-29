6d8cd0fbd89450900e578217e7e751c74cec78e4
Khoa Dien <khoa.dien@hotmail.com>
2024-08-29 13:14:55 -0300

    nvim: Replace deprecated LSP API

--- nvim/lua/plugins/code.lua
+++ nvim/lua/plugins/code.lua
@@ -20,7 +20,7 @@ return {
       {
         '<leader>ab',
         function()
-          vim.lsp.stop_client(vim.lsp.get_active_clients({ name = 'yamlls' }))
+          vim.lsp.stop_client(vim.lsp.get_clients({ name = 'yamlls' }))
           vim.opt.filetype = 'ansible'
           vim.opt.syntax = 'yaml.ansible'
         end,
