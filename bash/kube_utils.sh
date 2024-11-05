5852c03e109a9baaf9e2e3979b6de94f2ab1381b
Khoa Dien <khoa.dien@hotmail.com>
2024-11-05 11:23:06 -0400

    bash: improve kube utils
    - Add flag to unset kube context
    - Add function to set namespace

--- bash/kube_utils.sh
+++ bash/kube_utils.sh
@@ -21,17 +21,36 @@ fi
 kconf() {
   local config
   config=$(find "$HOME/.kube" -mindepth 1 -maxdepth 1 -type f -exec basename {} \; | fzf --prompt='Select kube config: ')
-  if [[ -n "$config" ]]; then
-    export KUBECONFIG="$HOME/.kube/$config"
-  fi
+  [[ -n "$config" ]] && export KUBECONFIG="$HOME/.kube/$config"
 }
 
+# shellcheck disable=SC2120
 kct() {
+  if [[ "$1" == '-u' ]]; then
+    kubectl config unset current-context
+    return $?
+  fi
+
   [[ -z "$KUBECONFIG" ]] && kconf
 
   local context
   context=$(kubectl config get-contexts --output name | fzf --prompt='Select kube context: ')
-  if [[ -n "$context" ]]; then
-    kubectl config use-context "$context"
+  [[ -n "$context" ]] && kubectl config use-context "$context"
+}
+
+kns() {
+  if [[ "$1" == '-u' ]]; then
+    local current_context
+    current_context=$(kubectl config current-context 2>/dev/null)
+    [[ -n "$current_context" ]] && kubectl config unset contexts."$current_context".namespace
+    return $?
+  fi
+
+  kubectl config current-context &>/dev/null || kct
+
+  if kubectl config current-context &>/dev/null; then
+    local namespace
+    namespace=$(kubectl get namespaces --output name | awk -F '/' '{print $2}' | fzf --prompt='Select namespace: ')
+    [[ -n "$namespace" ]] && kubectl config set-context --current --namespace="$namespace"
   fi
 }
