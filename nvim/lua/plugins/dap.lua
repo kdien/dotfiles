a0021ad212d64d198ff1098a5b2e0877ecea5361
Khoa Dien <khoa.dien@hotmail.com>
2025-03-06 10:14:45 -0400

    nvim: add keymap to debug Go tests

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -73,6 +73,7 @@ return {
     ft = 'go',
     config = function()
       require('dap-go').setup()
+      vim.keymap.set('n', '<leader>dt', "<Cmd>lua require('dap-go').debug_test()<CR>")
     end,
   },
 
