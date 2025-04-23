1352c34f354ef3cd7f9773dd83c753cd3b2307e8
Khoa Dien <khoa.dien@hotmail.com>
2025-04-22 23:27:24 -0300

    Revert "nvim: new colorscheme vague.nvim"
    This reverts commit 03f884575b8608a01463f5cf6de314569b2f7ca0.

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -2,31 +2,7 @@ vim.o.background = os.getenv('TERMINAL_THEME') or 'dark'
 
 return {
   {
-    'vague2k/vague.nvim',
-    config = function()
-      require('vague').setup({
-        transparent = true,
-      })
-      vim.cmd([[
-        colorscheme vague
-        highlight Visual guifg=NONE guibg=#474e5d
-        highlight NormalFloat guibg=#3d4148
-        highlight FloatBorder guibg=#3d4148
-        highlight StatusLine guibg=#434354
-        highlight LspReferenceText guibg=#3f4653
-        highlight Comment guifg=#6f6f86
-        highlight DiagnosticUnderlineHint gui=underline
-        highlight DiagnosticUnderlineInfo gui=underline
-        highlight DiagnosticUnderlineWarn gui=underline
-        highlight DiagnosticUnderlineError gui=underline
-        highlight link BlinkCmpDoc BlinkCmpMenu
-      ]])
-    end,
-  },
-
-  {
     'navarasu/onedark.nvim',
-    enabled = false,
     config = function()
       require('onedark').setup({
         style = 'dark',
