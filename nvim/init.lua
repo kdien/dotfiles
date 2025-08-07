6d0574e266126415e272bcd04b666f0e3d2ed1ea
Khoa Dien <khoa.dien@hotmail.com>
2025-08-07 12:13:55 -0300

    nvim: add step to load local config

--- nvim/init.lua
+++ nvim/init.lua
@@ -10,3 +10,17 @@ vim.cmd('colorscheme onedark')
 for _, group in ipairs(vim.fn.getcompletion('@lsp', 'highlight')) do
   vim.api.nvim_set_hl(0, group, {})
 end
+
+-- Load local config
+local local_conf = vim.fn.expand('$HOME/.config/nvim.local')
+if vim.fn.isdirectory(local_conf) == 1 then
+  local files = vim.fn.globpath(local_conf, '*.lua', true, true)
+  if type(files) == 'table' then
+    for _, file in ipairs(files) do
+      local ok, err = pcall(dofile, file)
+      if not ok then
+        vim.notify('Error loading ' .. file .. ': ' .. err, vim.log.levels.ERROR)
+      end
+    end
+  end
+end
